d35de15eb823582b3ddfb0b68c8c0d2e633f9e53
iMAK73 <muginovak73@gmail.com>
2018-03-25 21:17:03 +0300

    InitialCommit

---
+++ prj_git_learn02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prj_git_learn02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
